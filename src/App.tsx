2788c6b552bac0dcbcf94c90de82dae42e53e2a7
Gathsara2002 <liviru2002mmg@gmail.com>
2024-02-03 10:12:44 +0530

    get user ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {CreateUser} from "./components/CreateUser";
+import {GetUser} from "./components/GetUser";
 
 function App() {
     return (
         <>
             <CreateUser></CreateUser>
+            <GetUser></GetUser>
         </>
     );
 }
---
+++ src/components/GetUser.tsx
@@ -0,0 +1,13 @@
+export const GetUser = () => {
+    return (
+    <div>
+        <h2>User Details</h2>
+        <div>
+            <p>ID :</p>
+            <p>Name: </p>
+            <p>Email: </p>
+            <p>Phone Number:</p>
+        </div>
+    </div>
+    );
+};
